dd9cad43e2925bfed497cab7c47f19cceb60a6d6
Sparke2021 <93227213+Sparke2021@users.noreply.github.com>
2023-04-17 16:41:55 +0100

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,6 +1,14 @@
 // Your web app's Firebase configuration
 //ADD YOUR FIREBASE LINKS HERE
-
+var firebaseConfig = {
+ apiKey: "AIzaSyAK_gg7lHoMJdVlisKbiUdJ35idJDg-P9s",
+ authDomain: "social-e5855.firebaseapp.com",
+ databaseURL: "https://social-e5585-default-rtdb.firebaseio.com",
+ projectId: "social-e5585",
+ storageBucket: "social-5855",
+ messagingSenderId: "671543009162",
+ appId: "1:671543009162:web:a881fdddf02bf05c08b273" 
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
